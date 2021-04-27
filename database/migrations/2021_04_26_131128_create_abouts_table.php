c616de177d7e9f652ff1b77d2d3b224387b878a2
Achraf <hardy1348@live.fr>
2021-04-27 09:44:25 +0200

    fact

--- database/migrations/2021_04_26_131128_create_abouts_table.php
+++ database/migrations/2021_04_26_131128_create_abouts_table.php
@@ -15,15 +15,19 @@ class CreateAboutsTable extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
+            $table->string('nom', 100);
+            $table->string('prenom', 100);
+            $table->string('titre', 100);
+            $table->string('description', 200);
+            $table->date('birthday');
+            $table->string('website', 100);
+            $table->string('phone');
+            $table->string('city', 100);
+            $table->integer('age');
+            $table->string('degree', 80);
+            $table->string('email', 100);
+            $table->string('freelance', 100);
             $table->timestamps();
-            $table->string('Birthday');
-            $table->string('Website');
-            $table->string('Phone');
-            $table->string('City');
-            $table->integer('Age');
-            $table->string('Degree');
-            $table->string('PhEmailone');
-            $table->string('Freelance');
         });
     }
 
